158cf0f5069be03b96fcd1101a1dcc9d676d3ea9
DESKTOP-GR6TN8K\Songsang C <natrada_chairat@cmu.ac.th>
2018-04-29 13:55:23 +0700

    add other html file

---
+++ src/app/student/fee-student/fee-student.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fee-student',
+  templateUrl: './fee-student.component.html',
+  styleUrls: ['./fee-student.component.css']
+})
+export class FeeStudentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
