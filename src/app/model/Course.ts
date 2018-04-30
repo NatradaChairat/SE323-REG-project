7ea0d4ee62107e4847a97959f4a5438ce535e65c
Siriganya Sensupa <siriganya_sen@cmu.ac.th>
2018-04-30 17:23:33 +0700

    add admin data

--- src/app/model/Course.ts
+++ src/app/model/Course.ts
@@ -1,4 +1,13 @@
 export class Course {
   courseId: string;
   courseName: string;
+  courseCredit: number;
+  courseSec: string;
+  courseSeat: number;
+  courseTeacher: string;
+  courseSemester: string;
+  courseYear: string;
+  courseTime: string;
+  courseDay: string;
+
 }
